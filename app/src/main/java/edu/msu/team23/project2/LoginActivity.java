51f1bf6014269a71673f8c43783837e247ad536b
Gregory Szczerba <szczerb7@msu.edu>
2020-11-22 19:48:23 -0500

    Implemented user login.

--- app/src/main/java/edu/msu/team23/project2/LoginActivity.java
+++ app/src/main/java/edu/msu/team23/project2/LoginActivity.java
@@ -2,13 +2,106 @@ package edu.msu.team23.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import edu.msu.team23.project2.cloud.Cloud;
+import edu.msu.team23.project2.cloud.models.CheckersResult;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final int MAX_USERNAME_LENGTH = 32;
+    private static final int MAX_PASSWORD_LENGTH = 16;
+    public static final String USERNAME = "LoginActivity.username";
+    public static final String PASSWORD = "LoginActivity.password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    /**
+     * Handler for when the login button in pressed.
+     * @param view View the event came from
+     */
+    public void onLogin(View view) {
+        final EditText usernameField = findViewById(R.id.loginUsername);
+        final EditText passwordField = findViewById(R.id.loginPassword);
+
+        // Only attempt a login if the input is valid
+        if (areFieldsValid(usernameField, passwordField)) {
+            // Activity thread is in
+            final LoginActivity activity = this;
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    // Attempt login call to the server
+                    Cloud cloud = new Cloud();
+                    final CheckersResult result = cloud.loginUser(usernameField.getText().toString(), passwordField.getText().toString());
+
+                    if (result != null && result.getStatus().equals(Cloud.STATUS_SUCCESS)) {
+                        // Login was successful, start the menu activity
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Intent intent = new Intent(activity, MenuActivity.class);
+                                intent.putExtra(USERNAME, usernameField.getText().toString());
+                                intent.putExtra(PASSWORD, passwordField.getText().toString());
+                                startActivity(intent);
+                            }
+                        });
+                    } else {
+                        // Login failed, show corresponding error
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (result != null && result.getMessage().equals(Cloud.PASSWORD_ERROR)) {
+                                    passwordField.setError(getResources().getString(R.string.password_error));
+                                } else {
+                                    usernameField.setError(getResources().getString(R.string.user_error));
+                                }
+                            }
+                        });
+                    }
+                }
+            }).start();
+        }
+    }
+
+    /**
+     * Handler for when the create user button is pressed.
+     * @param view View the event came from
+     */
+    public void onCreateUser(View view) {
+        // TODO
+    }
+
+    /**
+     * Are the username and password fields in the correct format?
+     * @return Are the username and password fields in the correct format?
+     */
+    private boolean areFieldsValid(EditText usernameField, EditText passwordField) {
+        boolean areFieldsValid = true;
+
+        if (usernameField != null && passwordField != null) {
+            String username = usernameField.getText().toString();
+            String password = passwordField.getText().toString();
+
+            if (password.length() < 1 || password.length() > MAX_PASSWORD_LENGTH) {
+                areFieldsValid = false;
+                passwordField.setError(getResources().getString(R.string.invalid_password_length_error));
+            }
+            if (username.length() < 1 || username.length() > MAX_USERNAME_LENGTH) {
+                areFieldsValid = false;
+                usernameField.setError(getResources().getString(R.string.invalid_username_length_error));
+            }
+        } else {
+            areFieldsValid = false;
+        }
+
+        return areFieldsValid;
     }
 }
--- app/src/main/java/edu/msu/team23/project2/cloud/Cloud.java
+++ app/src/main/java/edu/msu/team23/project2/cloud/Cloud.java
@@ -1,16 +1,19 @@
 package edu.msu.team23.project2.cloud;
 
+import java.io.IOException;
+
 import edu.msu.team23.project2.CheckersGame;
 import edu.msu.team23.project2.cloud.models.CheckersResult;
 import edu.msu.team23.project2.cloud.models.ConnectResult;
 import edu.msu.team23.project2.cloud.models.LoadResult;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 @SuppressWarnings("deprecation")
 public class Cloud {
     private static final String MAGIC = "7VpFwBLcKne5p0Uu";
-    private static final String BASE_URL = "https://webdev.cse.msu.edu/~szczerb7/cse476/step6/";
+    private static final String BASE_URL = "https://webdev.cse.msu.edu/~szczerb7/cse476/project2/";
     public static final String SAVE_STATE_PATH = "save-state.php";
     public static final String LOAD_STATE_PATH = "load-state.php";
     public static final String LOGIN_USER_PATH = "login-user.php";
@@ -18,6 +21,10 @@ public class Cloud {
     public static final String CONNECT_PATH = "connect.php";
     public static final String DISCONNECT_PATH = "disconnect.php";
     private static final String UTF8 = "UTF-8";
+    public static final String STATUS_SUCCESS = "success";
+    public static final String STATUS_FAILURE = "failure";
+    public static final String USER_ERROR = "user error";
+    public static final String PASSWORD_ERROR = "password error";
 
     private static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -26,61 +33,70 @@ public class Cloud {
 
     /**
      * Save the current state of the game to the server's database.
-     * @param userName Username of the client
+     * @param username Username of the client
      * @param password Password for the logged in user
      * @return Result of the save state call
      */
-    public CheckersResult saveState(String userName, String password, CheckersGame game) {
+    public CheckersResult saveState(String username, String password, CheckersGame game) {
         return new CheckersResult();
     }
 
     /**
      * Attempt to load the state of the game the user is in.
-     * @param userName Username of the client
+     * @param username Username of the client
      * @param password Password for the logged in user
      * @return Result of the load state call
      */
-    public LoadResult loadState(String userName, String password) {
+    public LoadResult loadState(String username, String password) {
         return new LoadResult();
     }
 
     /**
      * Login to a pre-existing user in the server database.
-     * @param userName Username of the client
+     * @param username Username of the client
      * @param password Password for the logged in user
      * @return Result of the login user call
      */
-    public CheckersResult loginUser(String userName, String password) {
-        return new CheckersResult();
+    public CheckersResult loginUser(String username, String password) {
+        CheckersService service = retrofit.create(CheckersService.class);
+        try {
+            Response<CheckersResult> response = service.loginUser(username, MAGIC, password).execute();
+            if (!response.isSuccessful()) {
+                return null;
+            }
+            return response.body();
+        } catch(IOException | RuntimeException e) {
+            return null;
+        }
     }
 
     /**
      * Create a new user in the server's database.
-     * @param userName Username of the client
+     * @param username Username of the client
      * @param password Password for the logged in user
      * @return Result of the create user call
      */
-    public CheckersResult createUser(String userName, String password) {
+    public CheckersResult createUser(String username, String password) {
         return new CheckersResult();
     }
 
     /**
      * Attempt to connect to a new game.
-     * @param userName Username of the client
+     * @param username Username of the client
      * @param password Password for the logged in user
      * @return Result of the connect call
      */
-    public ConnectResult connect(String userName, String password) {
+    public ConnectResult connect(String username, String password) {
         return new ConnectResult();
     }
 
     /**
      * Disconnect from the game the client is in.
-     * @param userName Username of the client
+     * @param username Username of the client
      * @param password Password for the logged in user
      * @return Result of the disconnect call
      */
-    public CheckersResult disconnect(String userName, String password) {
+    public CheckersResult disconnect(String username, String password) {
         return new CheckersResult();
     }
 }
